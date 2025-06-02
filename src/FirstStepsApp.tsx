47b0ee19566cf2dffc557629c8003de560a913df
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-02 12:04:04 -0400

    feat: update FirstStepsApp to display shopping cart items and implement ItemCounter component

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -1,14 +1,14 @@
+import { ItemCounter } from './shopping-cart/ItemCounter';
+
 export function FirstStepsApp() {
   return (
     <>
-      <h1>Hola Mundo!!</h1>
-      <p>Esto es un párrafo!!</p>
+      <h1>Carrito de compras</h1>
 
-      <button>Click me</button>
-
-      <div>
-        <h2>Hola dentro de un div</h2>
-      </div>
+      <ItemCounter name="Nintendo Switch 2" quantity={1} />
+      <ItemCounter name="Pro Controller" quantity={2} />
+      <ItemCounter name="Super Smash" quantity={3} />
+      <ItemCounter name="Super Mario" quantity={3} />
     </>
   );
 }
