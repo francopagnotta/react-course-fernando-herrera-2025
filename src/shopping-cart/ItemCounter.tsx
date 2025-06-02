19c71f3d51c3566e91d1ad0f3aa03a56b3a107af
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-02 13:21:26 -0400

    feat: enhance ItemCounter component with state management and styling

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,32 +1,47 @@
+import { useState } from 'react';
+
+// import './ItemCounter.css';
+import styles from './ItemCounter.module.css';
+
 interface Props {
   name: string;
   quantity?: number;
 }
 
-export const ItemCounter = ({ name, quantity }: Props) => {
-  const handleClick = () => {
-    console.log(`Click en ${name}`);
+export const ItemCounter = ({ name, quantity = 1 }: Props) => {
+  const [count, setCount] = useState(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubtract = () => {
+    if (count === 1) return;
+
+    setCount(count - 1);
   };
 
   return (
     <section
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 10,
-        marginTop: 10,
-      }}
+      className={styles.itemRow}
+      // style={{
+      //   display: 'flex',
+      //   alignItems: 'center',
+      //   gap: 10,
+      //   marginTop: 10,
+      // }}
     >
       <span
+        className={styles['item-text']}
         style={{
-          width: 150,
+          color: count === 1 ? 'red' : 'black',
         }}
       >
         {name}
       </span>
-      <button onClick={handleClick}>+1</button>
-      <span>{quantity}</span>
-      <button>-1</button>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleSubtract}>-1</button>
     </section>
   );
 };
